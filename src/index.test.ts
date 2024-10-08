57e9e5eb8e79de8298412ebac367519c80e6f05e
Olly Pethick <olly.pethick@focus-internal.co.uk>
2024-10-08 14:19:05 +0100

    Reset quantities between tests and add new test

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,6 +1,13 @@
 import { AmountOfDenom, initialAmountOfDenom } from "./constants/denominations"
 import { calculateChange } from "./index"
 
+beforeEach(() => {
+  // Reset the quantity of each denomination to 0
+  Object.values(initialAmountOfDenom).forEach((denom) => {
+    denom.quantity = 0
+  })
+})
+
 describe("calculateChange", () => {
   it("should return correct change for given amount and total", () => {
     const given = 20
@@ -8,9 +15,9 @@ describe("calculateChange", () => {
     const expectedChange: AmountOfDenom = {
       ...initialAmountOfDenom,
       200: { ...initialAmountOfDenom[200], quantity: 1 },
+      100: { ...initialAmountOfDenom[100], quantity: 1 },
       20: { ...initialAmountOfDenom[20], quantity: 1 },
       2: { ...initialAmountOfDenom[2], quantity: 2 },
-      1: { ...initialAmountOfDenom[1], quantity: 2 },
     }
 
     expect(calculateChange(given, total)).toEqual(expectedChange)
@@ -26,7 +33,22 @@ describe("calculateChange", () => {
       20: { ...initialAmountOfDenom[20], quantity: 1 },
       5: { ...initialAmountOfDenom[5], quantity: 1 },
       2: { ...initialAmountOfDenom[2], quantity: 2 },
-      1: { ...initialAmountOfDenom[1], quantity: 2 },
+    }
+
+    expect(calculateChange(given, total)).toEqual(expectedChange)
+  })
+
+  it("should return correct change for another given amount and total", () => {
+    const given = 20
+    const total = 8.04
+    const expectedChange: AmountOfDenom = {
+      ...initialAmountOfDenom,
+      1000: { ...initialAmountOfDenom[1000], quantity: 1 },
+      100: { ...initialAmountOfDenom[100], quantity: 1 },
+      50: { ...initialAmountOfDenom[50], quantity: 1 },
+      20: { ...initialAmountOfDenom[20], quantity: 2 },
+      5: { ...initialAmountOfDenom[5], quantity: 1 },
+      1: { ...initialAmountOfDenom[1], quantity: 1 },
     }
 
     expect(calculateChange(given, total)).toEqual(expectedChange)
@@ -48,7 +70,6 @@ describe("calculateChange", () => {
     const expectedChange: AmountOfDenom = {
       ...initialAmountOfDenom,
       2: { ...initialAmountOfDenom[2], quantity: 1 },
-      1: { ...initialAmountOfDenom[1], quantity: 1 },
     }
 
     expect(calculateChange(given, total)).toEqual(expectedChange)
